c70a4067001dd2c883637877ab83b25ebe9cd22f
donalex21 <lang.learn21@gmai.com>
2021-09-20 13:52:42 +0300

    ANgular interface implementation

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -7,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
-  hero = 'Windstorm';
+  hero: Hero = {
+     id: 1,
+     name: 'Windstorm'
+  };
 
   constructor() { }
 
