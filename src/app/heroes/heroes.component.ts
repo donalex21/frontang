33986acb3840c303b086c5c52606faa671f51a5d
donalex21 <lang.learn21@gmai.com>
2021-09-23 15:13:16 +0300

    Messaging logic implementation

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,19 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
-import { HeroService } from '../hero.service';
+import { Hero }              from '../hero';
+import { HEROES }            from '../mock-heroes';
+import { HeroService }       from '../hero.service';
+import { MessageService }    from '../message.service';
 
 @Component({
-  selector: 'app-heroes',
+  selector:    'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls:   ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
 
   heroes: Hero[] = [];
   selectedHero?: Hero;
 
-  constructor(private heroService: HeroService) { }
+  constructor(private heroService:    HeroService,
+	      private messageService: MessageService) { }
 
   ngOnInit(): void {
     this.getHeroes();
@@ -21,6 +23,7 @@ export class HeroesComponent implements OnInit {
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add("Heroes Component: Selected Hero id=${hero.id}");
   }
 
   getHeroes(): void {
