1aef123333483b5e4658e6622c5d8e7f1dc37b95
donalex21 <lang.learn21@gmai.com>
2021-09-27 15:13:39 +0300

    Add hero detail navigation

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -12,18 +12,11 @@ import { MessageService }    from '../message.service';
 export class HeroesComponent implements OnInit {
 
   heroes: Hero[] = [];
-  selectedHero?: Hero;
 
-  constructor(private heroService:    HeroService,
-	      private messageService: MessageService) { }
+  constructor(private heroService:    HeroService) { }
 
   ngOnInit(): void {
     this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`Heroes Component: Selected Hero id=${hero.id}`);
   }
 
   getHeroes(): void {
