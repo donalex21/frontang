52a0e56e1cfb4b44568992aad6e28d8d4f605150
donalex21 <lang.learn21@gmai.com>
2021-09-27 15:29:54 +0300

    Add constant routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 
+const routes: Routes = [
+  {
+    path: 'detail/:id',
+    component: HeroDetailComponent
+  }
+]
 
 @NgModule({
   declarations: [],
